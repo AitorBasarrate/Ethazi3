09803466201d96781e38415efa1771d858e6703e
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-17 13:11:46 +0100

    Primer Commit

---
+++ Ethazi3/src/Controlador/Aplikazioa.java
@@ -0,0 +1,13 @@
+package Controlador;
+
+import Modelo.DB;
+
+public class Aplikazioa {
+
+	public static void main(String[] args) {
+		String basedatos = "ethazi3";
+		DB con1 = new DB(basedatos);
+		con1.getConexion();
+	}
+
+}
---
+++ Ethazi3/src/Modelo/DB.java
@@ -0,0 +1,44 @@
+package Modelo;
+ 
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.SQLException;
+	 
+	public class DB {
+	    private String  maquina     = "localhost/ethazi3";
+	    private String  usuario     = "root";
+	    private String  clave       = "";
+	    private int puerto          = 3306;
+	    private String  servidor    = "";
+	    private static Connection conexion  = null;
+	 
+	    //CONSTRUCTOR
+	    //Recibe el nombre de la base de datos
+	    public DB(String baseDatos){
+	        this.servidor="jdbc:mysql://"+this.maquina+":"+
+	                        this.puerto+"/"+baseDatos;
+	 
+	        //Registrar el driver
+	        try {
+	            Class.forName("com.mysql.jdbc.Driver");
+	        } catch (ClassNotFoundException e) {
+	            System.err.println("ERROR AL REGISTRAR EL DRIVER");
+	            System.exit(0); //parar la ejecución
+	        }
+	 
+	        //Establecer la conexión con el servidor
+	        try {
+	            conexion = DriverManager.getConnection("jdbc:mysql://" + this.maquina, this.usuario, this.clave);
+	        } catch (SQLException e) {
+	            System.err.println("ERROR AL CONECTAR CON EL SERVIDOR");
+	            System.exit(0); //parar la ejecución
+	        }
+	        System.out.println("Conectado a "+baseDatos);
+	    }
+	 
+	    //Devuelve el objeto Connection que se usará en la clase Controller
+	    public static Connection getConexion() {
+	        return conexion;
+	    }
+	 
+	}
