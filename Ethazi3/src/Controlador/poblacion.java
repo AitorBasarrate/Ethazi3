dec9a6ae2b29505c7116b36332ff29be588e2af2
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-21 12:04:47 +0100

    clase guztiak eginda

---
+++ Ethazi3/src/Controlador/poblacion.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+public class poblacion {
+	private int Cod_postal;
+	private String Nombre;
+	
+	public poblacion (int Cod_postal, String Nombre) {
+		
+	}
+}
---
+++ Ethazi3/src/Controlador/poblacion_parada.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+public class poblacion_parada {
+	private int Cod_parada;
+	private int Cod_postal;
+	
+	public poblacion_parada (int Cod_parada, int Cod_postal) {
+		
+	}
+}
