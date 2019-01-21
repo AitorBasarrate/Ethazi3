5d89ec1621551f46923ab0d577233e39b768c973
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-21 11:51:34 +0100

    linea_autobus clasea sortuta.

---
+++ Ethazi3/src/Controlador/linea_autobus.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+public class linea_autobus {
+	private String Cod_linea;
+	private int Cod_autobus;
+	
+	public linea_autobus (String Cod_linea, int Cod_autobus) {
+		
+	}
+}
