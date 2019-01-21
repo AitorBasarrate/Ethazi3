0531b8cfc5e883514f740ee5142f466d4f5fabe4
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-21 11:58:21 +0100

    parada clasea sortuta

---
+++ Ethazi3/src/Controlador/parada.java
@@ -0,0 +1,13 @@
+package Controlador;
+
+public class parada {
+	private int Cod_parada;
+	private String Nombre;
+	private String Calle;
+	private int Latitud;
+	private int Longitud;
+	
+	public parada (int Cod_parada, String Nombre, String Calle, int Latitud, int Longitud) {
+		
+	}
+}
