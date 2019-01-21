9f369b66848b818e31e4faae77b5e4ebe9d2ee70
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-21 10:37:17 +0100

    Billete clasea sortuta.

---
+++ Ethazi3/src/Controlador/Billete.java
@@ -0,0 +1,20 @@
+package Controlador;
+
+import java.util.*;
+
+public class Billete {
+	private String Cod_billete;
+	private String NTrayecto;
+	private String Cod_Linea;
+	private String Cod_Bus;
+	private String Cod_Parada_Inicio;
+	private String Cod_Parada_Fin;
+	private Date fecha;
+	private String hora;
+	private String Dni;
+	private int Precio;
+	
+	public Billete (String Cod, String NTrayecto, String Cod_Linea, String Cod_Bus, String Cod_Parada_Inicio, String Cod_Parada_Fin, Date fecha, String hora, String Dni, int Precio) {
+		
+	}
+}
