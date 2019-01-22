cb923cb37d5ad4c94c7d07376d500851abdedbc3
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-22 12:35:23 +0100

    ff

--- Ethazi3/src/Vista/Frame.java
+++ Ethazi3/src/Vista/Frame.java
@@ -1,10 +1,6 @@
 package Vista;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
