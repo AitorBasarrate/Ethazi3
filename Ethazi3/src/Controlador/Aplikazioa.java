2925b5bc2e2c29d73d9735f5257b57ef5de6e096
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-01-30 09:10:05 +0100

    kk

--- Ethazi3/src/Controlador/Aplikazioa.java
+++ Ethazi3/src/Controlador/Aplikazioa.java
@@ -1,48 +1,139 @@
 package Controlador;
 
-import java.awt.EventQueue;
-import java.awt.Frame;
-import java.beans.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.w3c.dom.CDATASection;
+import java.util.concurrent.TimeUnit;
 
 import Modelo.DB;
-
+import Modelo.Kontsulta;
 
 public class Aplikazioa {
 
+	
+
 	public static void main(String[] args) {
-		Vista.Frame ejecutar= new Vista.Frame();
+		
+		String basedatos = "ethazi3";
+		DB con1 = new DB(basedatos);
+		con1.getConexion();
+
+//		Metodoak.muestraDatos(Cod_linea, Nombre);
+//		Kontsulta.guardaCliente();
+//		Metodoak.nanKomprobaketa();
+		String imprimaki = Kontsulta.lineaAukeratu();
+		System.out.println(imprimaki);
+
+		Vista.Lehena ejecutar= new Vista.Lehena();
 		ejecutar.setVisible(true);
-		
-<<<<<<< HEAD
-=======
-		
-		  CDATASection conexion = null;
->>>>>>> branch 'master' of https://github.com/AitorBasarrate/Ethazi3.git
 	}
-<<<<<<< HEAD
+	
+	
+
+	
+
+	
+
+	public static String[] diruarenBueltak(String emaitza, String ordainketa) {// emaitza balioa main-etik hartu
+		double DoubleTextDiruTot = Double.parseDouble(emaitza);
+		double DoubleTextOrdainketa = Double.parseDouble(ordainketa);
+
+		double[] array = new double[14];
+		// Dena zentimoetan jarri
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 5;
+		array[3] = 10;
+		array[4] = 20;
+		array[5] = 50;
+		array[6] = 100;
+		array[7] = 200;
+		array[8] = 500;
+		array[9] = 1000;
+		array[10] = 2000;
+		array[11] = 5000;
+		array[12] = 10000;
+		array[13] = 20000;
+		double bueltak;
+		String[] arrayBueltak = new String[10];
+		double buelta;
+		String bueltaString;
+		int j = 0;
+
+		// double ordainketa;
+
+		// System.out.println("Sartu dirua: ");
+		// ordainketa = sc.nextDouble();
+
+		do {
+			if (DoubleTextOrdainketa < 0) {
+				System.out.println("Sartu zenbaki positibo bat:");
+
+			}
+		} while (DoubleTextOrdainketa < 0);
+
+		bueltak = DoubleTextOrdainketa - DoubleTextDiruTot;
+
+		// Begiratu ea diru nahiko sartu duen edo bueltak eman behar diogun
+
+		if (bueltak < 0) {
+			arrayBueltak[0] = "";
+			/*
+			 * do { bueltak=-bueltak;
+			 * System.out.println("Falta zaizkizu: "+bueltak+" euro");
+			 * System.out.println("Sartu falta den dirua:"); emaitza=bueltak;
+			 * 
+			 * do { if (ordainketa<0) { System.out.println("Sartu zenbaki positibo bat:");
+			 * 
+			 * } }while (ordainketa<0); bueltak=ordainketa-emaitza; }while (bueltak!=0 &&
+			 * bueltak < 0);//errepikatu 0 baino handiagoa edo berdina den zenbaki bat sartu
+			 * harte
+			 */
+		}
+		// galdetu bueltak dauden ala ez
+		if (bueltak == 0) {
+			System.out.println("Ez daude bueltak");
+			arrayBueltak[0] = "0";
+		} else
+			System.out.println("Bueltak:");
+		// Bueltak eman
+
+		if (bueltak > 0) {
+			bueltak = bueltak * 100;// bueltak zentimoetan jarri
+			do {
+				// begiratu noiz bueltak handiagoak diren, horrela bueltei zenbaki hurbilena
+				// pantailaratuko du
+				for (int i = 13; i >= 0; i--) {
+
+					if (bueltak >= array[i]) {
+						bueltak = bueltak - array[i];// bueltei kendu pantailaratuko den balioa
+						buelta = array[i] / 100;
+						bueltaString = Double.toString(buelta);
+						arrayBueltak[j] = bueltaString;
+						System.out.println(arrayBueltak[j]);
+						j++;
+						break;
+					}
+				}
+			} while (bueltak != 0);// errepikatu prozesu hau bueltak 0 izan harte
+
+		}
+
+		return arrayBueltak;// main-era bidali buelten zerrenda
+	}/* metodo bueltak */
+
+	public static String kenketa(String diruTot, String ordainketa) {
+
+		double doubleDiruTot = Double.parseDouble(diruTot);
+		double doubleOrdainketa = Double.parseDouble(ordainketa);
+		double emaitza = doubleDiruTot - doubleOrdainketa;
+		String stringEmaitza = Double.toString(emaitza);
+		return stringEmaitza;
+	}
+
+	public static void bostSegundo() {
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
-	
-		//String basedatos = "ethazi3";
-		//DB con1 = new DB(basedatos);
-		//con1.getConexion();
-		
-		
-		  //CDATASection conexion = null;
-//		  
-//		  
-//		  
-//		  
-//		  
-
-	
-	
-=======
-    
->>>>>>> branch 'master' of https://github.com/AitorBasarrate/Ethazi3.git
-
--- Ethazi3/src/Vista/Frame.java
+++ Ethazi3/src/Vista/Frame.java
@@ -1,21 +1,31 @@
 package Vista;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import Controlador.Aplikazioa;
+
 public class Frame extends JFrame {
 
 	private JPanel contentPane;
-	private static final long serialVersionUID = 1L;
+//	private static final long serialVersionUID = 1L;
+////	
+//	GridBagConstraints config = new GridBagConstraints();
 	
-	GridBagConstraints config = new GridBagConstraints();
-	
-	static Ventana1 Ventana1 = new Ventana1();
-	static Ventana2 Ventana2 = new Ventana2();
-	
+//	//static Lehena Lehena = new Lehena();
+//	//static SaioaHasi SaioaHasi = new SaioaHasi();
+//	static Lineak Lineak = new Lineak();
+//	static Geltokiak Geltokiak = new Geltokiak();
+//	static Ordainketa Ordainketa = new Ordainketa();
+//	static AteraBilletea AteraBilletea = new AteraBilletea();
+//	static Azkena Azkena = new Azkena();
 
 	/**
 	 * Launch the application.
@@ -26,36 +36,51 @@ public class Frame extends JFrame {
 	 * Create the frame.
 	 */
 	public Frame() {
-		this.setSize(770,670);  
-		this.setLocationRelativeTo(null);  
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
+//		this.setSize(478,300);  
+//		this.setLocationRelativeTo(null);  
+//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		setBounds(0, 0, 635, 455);
 		
-		Ventana1.setBounds(0, 0, 434, 262);
-		contentPane.add(Ventana1);
-		Ventana1.setVisible(true);
-////		Ventana1.addMouseListener(new MouseAdapter() {
-////	   	 @Override
-////	   	 public void mouseClicked(MouseEvent arg0) {
-////	   		 if(Ventana1.isVisible()){
-////	       	 
-////	        	Ventana2.setVisible(true);
-////	        	
-////	        	Ventana1.setVisible(false);           	 
-////	   	 
-////	   		 }   	 
-//	   		 
-//	   	// }
-//		});
-		Ventana2.setBounds(0, 0, 434, 262);
-		getContentPane().add(Ventana2);
-		Ventana2.setLayout(null);
-		Ventana2.setVisible(false);
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		setContentPane(contentPane);
+//		contentPane.setLayout(null);
+		
+//		Lehena.setBounds(0, 0, 700, 500);
+//		contentPane.add(Lehena);
+//		Lehena.setVisible(true);
+		
+//		SaioaHasi.setBounds(0, 0, 700, 500);
+//		getContentPane().add(SaioaHasi);
+//		SaioaHasi.setLayout(null);
+//		SaioaHasi.setVisible(false);
+//		
+//		Lineak.setBounds(0, 0, 700, 500);
+//		getContentPane().add(Lineak);
+//		Lineak.setLayout(null);
+//		Lineak.setVisible(false);
+//		
+//		Geltokiak.setBounds(0, 0, 700, 500);
+//		getContentPane().add(Geltokiak);
+//		Geltokiak.setLayout(null);
+//		Geltokiak.setVisible(false);
+//		
+//		Ordainketa.setBounds(0, 0, 700, 500);
+//		getContentPane().add(Ordainketa);
+//		Ordainketa.setLayout(null);
+//		Ordainketa.setVisible(false);
+//		
+//		AteraBilletea.setBounds(0, 0, 700, 500);
+//		getContentPane().add(AteraBilletea);
+//		AteraBilletea.setLayout(null);
+//		AteraBilletea.setVisible(false);
+//		
+//		Azkena.setBounds(0, 0, 700, 500);
+//		getContentPane().add(Azkena);
+//		Azkena.setLayout(null);
+//		Azkena.setVisible(false);
+		
 		
 	}
 
