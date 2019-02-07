d06ea337fd1109778d7a0ded3c1ec6f1013c1604
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-02-07 14:14:27 +0100

    ZDTJAZTZRGNJD<

--- src/Controlador/Aplikazioa.java
+++ src/Controlador/Aplikazioa.java
@@ -19,23 +19,8 @@ public class Aplikazioa {
 		con1.getConexion();
 		
 
-		System.out.println("Empieza el metodo ");// COMPROBACION PARA EL METODO
-
-
+		System.out.println("Empieza el metodo ");// COMPROBACION PARA EL METODO.
 		
-//		Metodoak.nanKomprobaketa(null);
-//		int mini = 1001;
-//		int maxi = 1002;
-//		int rndm = Metodoak.generaNumeroAleatorio(mini, maxi);
-//		System.out.println(rndm);
-
-//		Metodoak.haversineMetodo();
-//		Metodoak.pasahitzaKomprobaketa();
-
-//		Kontsulta.guardaCliente();
-
-//		Kontsulta.muestraParada();
-
 
 		Vista.Lehena ejecutar = new Vista.Lehena();
 		ejecutar.setVisible(true);
--- src/Controlador/FitxategiRW.java
+++ src/Controlador/FitxategiRW.java
@@ -1,14 +1,42 @@
 package Controlador;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 
 public class FitxategiRW {
-	File ticket = new File ("../Ethazi3/src/Controlador/ticket");
-	FileReader fr = new FileReader (ticket);
-	BufferedReader br = new BufferedReader(fr);
-	...
-	String linea = br.readLine();
-}
+	File fitxategi = null;
+	FileReader fr = null;
+	BufferedReader br = null;
+	
+	{
+
+	try
+	{
+		// Apertura del fichero y creacion de BufferedReader para poder
+		// hacer una lectura comoda (disponer del metodo readLine()).
+		fitxategi = new File("../Ethazi3/src/Controlador/ticket");
+		fr = new FileReader(fitxategi);
+		br = new BufferedReader(fr);
+
+		// Lectura del fichero
+		String linea;
+		while ((linea = br.readLine()) != null)
+			System.out.println(linea);
+	}catch(
+	Exception e)
+	{
+		e.printStackTrace();
+	}finally
+	{
+		// En el finally cerramos el fichero, para asegurarnos
+		// que se cierra tanto si todo va bien como si salta
+		// una excepcion.
+		try {
+			if (null != fr) {
+				fr.close();
+			}
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	}
+}}
 //http://chuwiki.chuidiang.org/index.php?title=Lectura_y_Escritura_de_Ficheros_en_Java
