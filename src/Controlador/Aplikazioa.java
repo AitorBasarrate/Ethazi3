4e3cbc8e2a6ad87f8aa78804ac336fa52500d73b
AitorBasarrate <aitor.basarrateas@elorrieta-errekamari.com>
2019-02-08 09:50:58 +0100

    kkkkkkkkkkkkkkkkkkkkkk

--- src/Controlador/Aplikazioa.java
+++ src/Controlador/Aplikazioa.java
@@ -6,7 +6,7 @@ import Modelo.DB;
 public class Aplikazioa {
 
 	public static void main(String[] args) {
-
+		System.out.println("Pito");
 		String basedatos = "ethazi3";
 		DB con1 = new DB(basedatos);
 		con1.getConexion();
